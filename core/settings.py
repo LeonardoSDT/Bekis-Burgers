0c033afab5836c7f06ff7d4e98e40017c25637ba
Leonardo Sánchez Del Toro <leonardo.sanchez@upnay.edu.mx>
2023-02-19 20:53:45 -0700

    Added logout button

--- core/settings.py
+++ core/settings.py
@@ -56,8 +56,8 @@ MIDDLEWARE = [
 ]
 
 ROOT_URLCONF = 'core.urls'
-LOGIN_REDIRECT_URL = "home"  # Route defined in home/urls.py
-LOGOUT_REDIRECT_URL = "home"  # Route defined in home/urls.py
+LOGIN_REDIRECT_URL = "index"  # Route defined in home/urls.py
+LOGOUT_REDIRECT_URL = "index"  # Route defined in home/urls.py
 TEMPLATE_DIR = os.path.join(CORE_DIR, "apps/templates")  # ROOT dir for templates
 
 TEMPLATES = [
