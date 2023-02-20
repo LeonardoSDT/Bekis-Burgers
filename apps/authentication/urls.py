88b9b0bda7f5b71af13eb4152df26c727e84b085
Leonardo Sánchez Del Toro <leonardo.sanchez@upnay.edu.mx>
2023-02-19 20:22:32 -0700

    Added template structure
    - Added template structure.
    - Added login and register views.

--- apps/authentication/urls.py
+++ apps/authentication/urls.py
@@ -1,7 +1,11 @@
 from django.urls import path
+from .views import login_view, register_user
+from django.contrib.auth.views import LogoutView
 
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='index'),
+    path('login/', login_view, name="login"),
+    path('register/', register_user, name="register"),
+    path("logout/", LogoutView.as_view(), name="logout")
 ]
--- apps/authentication/views.py
+++ apps/authentication/views.py
@@ -1,9 +1,52 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+
+from django.contrib.auth import authenticate, login
+from apps.authentication.forms import LoginForm, SignUpForm
 
 # Create your views here.
 
-from django.http import HttpResponse
+def login_view(request):
+    form = LoginForm(request.POST or None)
+
+    msg = None
+
+    if request.method == "POST":
+
+        if form.is_valid():
+            username = form.cleaned_data.get("username")
+            password = form.cleaned_data.get("password")
+            user = authenticate(username=username, password=password)
+            if user is not None:
+                login(request, user)
+                return redirect("/")
+            else:
+                msg = 'Credenciales invalidas.'
+        else:
+            msg = 'Error validando el formulario.'
+
+    return render(request, "accounts/login.html", {"form": form, "msg": msg})
 
 
-def index(request):
-    return HttpResponse("Hello, world. You're at the authentication index.")
+def register_user(request):
+    msg = None
+    success = False
+
+    if request.method == "POST":
+        form = SignUpForm(request.POST)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data.get("username")
+            raw_password = form.cleaned_data.get("password1")
+            user = authenticate(username=username, password=raw_password)
+
+            msg = 'Usuario creado exitosamente.'
+            success = True
+
+            # return redirect("/login/")
+
+        else:
+            msg = 'El formulario no es válido.'
+    else:
+        form = SignUpForm()
+
+    return render(request, "accounts/register.html", {"form": form, "msg": msg, "success": success})
