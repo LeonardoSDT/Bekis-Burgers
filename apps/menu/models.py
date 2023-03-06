1aa6091f6c52b779f9bd16f98e70b3a02dbb0fc9
Leonardo Sánchez Del Toro <leonardo.sanchez@upnay.edu.mx>
2023-03-06 13:15:10 -0700

    Added __str__ method to the model Product

--- apps/menu/models.py
+++ apps/menu/models.py
@@ -6,3 +6,6 @@ class Product(models.Model):
     name = models.CharField(max_length=40, default='', blank=False)
     description = models.CharField(max_length=200, default='', blank=False)
     img_url = models.CharField(max_length=40, default='', blank=False)
+
+    def __str__(self):
+        return self.name
